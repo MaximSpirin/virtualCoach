99332b3c2abd3937f6d8f1e03ee30fbc36300b43
Maxim <maximspirin@gmail.com>
2015-10-02 00:24:04 +0300

    progress on ToolPanel

--- js/view/ui/ToolsPanel.js
+++ js/view/ui/ToolsPanel.js
@@ -9,6 +9,8 @@
     ToolsPanel.prototype.componentHeight;
     ToolsPanel.prototype.backButton;
     ToolsPanel.prototype.saveButton;
+    ToolsPanel.prototype.constantButtonsSet;
+    ToolsPanel.prototype.dynamicButtonsSet;
 
     //static variable
     ToolsPanel.PANEL_STD_HEIGHT = 50;
@@ -19,7 +21,7 @@
         this.Container_constructor();
         this.componentWidth = width;
         this.componentHeight = height;
-
+        
         this.initialize();
     }
 
@@ -30,6 +32,9 @@
         this.background.graphics.beginFill("#dddddd").drawRect(0, 0, this.componentWidth, this.componentHeight);
         this.addChild(this.background);
 
+        //init dynamic buttons array
+        this.dynamicButtonsSet = [];
+        
         this.saveButton = new SimpleTextButton("Save", "20px Arial", "#000000", "#FFFFFF", "#999999", "#0000FF", 70);
         this.saveButton.x = 10;
         this.saveButton.y = 5;
@@ -40,8 +45,16 @@
         this.backButton.y = 5;
         this.backButton.on("click",backClickListener,this);
         this.addChild(this.backButton);
+        
+        Dispatcher.getInstance().on(ApplicationEvent.ELEMENT_SELECTED, elementSelectedHandler, this);
     };
 
+    /********************************* event listeners **********************************/
+    
+    function elementSelectedHandler(evt){
+            
+    }
+    
     function backClickListener(){
         Dispatcher.getInstance().dispatchEvent(new ApplicationEvent(ApplicationEvent.NAVIGATE_BACK));
     }
