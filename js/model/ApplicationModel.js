5dc5723ac10698bd149beb46a5018e3e67e6b861
Maxim Spirin <maximspirin@gmail.com>
2015-10-21 18:56:31 +0300

    version number update

--- js/model/ApplicationModel.js
+++ js/model/ApplicationModel.js
@@ -11,7 +11,7 @@
 
 
     //static variables and constants
-    ApplicationModel.VERSION = "0.1.0";
+    ApplicationModel.VERSION = "0.1.1";
     ApplicationModel.debugVersion = false;
     ApplicationModel.instance = null;
     ApplicationModel.APP_WIDTH = 800;
